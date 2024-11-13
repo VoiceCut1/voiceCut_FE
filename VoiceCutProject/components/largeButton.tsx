b81d431c0970619c64ebcf7bfdd03242a7ca6634
RecyclingBottle <hoholeo382@gmail.com>
2024-11-13 14:28:00 +0900

    Feat: 페이지 이동 및 보호자 번호 목록 페이지 구현

--- VoiceCutProject/components/largeButton.tsx
+++ VoiceCutProject/components/largeButton.tsx
@@ -1,9 +1,18 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import FontText from './fontText';
+import {useNavigation, NavigationProp} from '@react-navigation/native';
+import {StackParamList} from '../constans/interface'; // 페이지 이동을 위한 인터페이스 불러오기
 
-const LargeButton = () => {
+const LargeButton: React.FC = () => {
+  const navigation = useNavigation<NavigationProp<StackParamList>>();
+
+  const handlePress = () => {
+    navigation.navigate('NokListPage'); // NokListPage로 이동
+  };
+
   return (
-    <TouchableOpacity style={styles.addButton}>
+    <TouchableOpacity style={styles.addButton} onPress={handlePress}>
       <FontText size={35} color="white">
         보호자 번호{'\n'}등록하기
       </FontText>
