7402bc1c8df98d4cdd0bd62d042bc279f69d3c65
jeon-eunju <lucy726jeon@gmail.com>
2024-11-13 18:16:29 +0900

    Style : main 페이지에 방패 이미지 추가

--- VoiceCutProject/screens/main.tsx
+++ VoiceCutProject/screens/main.tsx
@@ -1,12 +1,16 @@
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Image} from 'react-native';
 import FontText from '../components/fontText';
 import NumberList from '../components/numberList';
 
 const MainPage = () => {
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../assets/img/shield.png')}
+        style={styles.image}
+      />
       <FontText size={40}>
-        현재{'\n'}보이스피싱 {'\n'}
+        보이스피싱{'\n'}
         <Text style={styles.orangeText}>탐지중</Text>
       </FontText>
 
@@ -24,7 +28,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: '20%',
+    position: 'relative',
+    paddingTop: '25%',
   },
   orangeText: {
     color: '#F68E5F',
@@ -33,11 +38,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     borderRadius: 10,
+    marginTop: '15%',
     margin: 10,
     padding: '3%',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: '50%',
+    height: '30%',
+    position: 'absolute',
+    left: '25%',
+    top: '5%',
   },
 });
 
