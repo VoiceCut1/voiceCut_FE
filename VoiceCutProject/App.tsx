6781ce2dbc730a9d27eac7ebf0f3c238852f7caa
jeon-eunju <lucy726jeon@gmail.com>
2024-11-16 01:41:15 +0900

    Feat : 푸시알림 구현 (Firebase 테스트 메세지로 알림 들어오는거까지 확인)

--- VoiceCutProject/App.tsx
+++ VoiceCutProject/App.tsx
@@ -5,6 +5,8 @@ import {createStackNavigator} from '@react-navigation/stack';
 import MainPage from './screens/main';
 import NokListPage from './screens/nokListPage';
 import {StackParamList} from './constans/interface';
+import {Alert} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const Stack = createStackNavigator<StackParamList>();
 
@@ -14,6 +16,18 @@ function App(): React.JSX.Element {
       PERMISSIONS.ANDROID.READ_CONTACTS,
       PERMISSIONS.ANDROID.RECORD_AUDIO,
     ]);
+  }, []);
+
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('백그라운드에서 알림 수신 확인');
+  });
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('알림 도착!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
   }, []);
 
   return (
